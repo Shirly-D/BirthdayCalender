f123f51239619ecc9075720fb1215ad64af087d0
Shirly <shirlyd@axioned.com>
2021-10-01 14:54:43 +0530

    json data of birthdays added

---
+++ assets/js/jsonData.js
@@ -0,0 +1,48 @@
+let birthdayList = [
+    { 
+        "name": "Tyrion Lannister", 
+        "birthday": "12/02/1978" 
+    },
+    { 
+        "name": "Cersei Lannister", 
+        "birthday": "11/30/1975" 
+    },
+    { 
+        "name": "Daenerys Targeryen", 
+        "birthday": "11/24/1991" 
+    },
+    { 
+        "name": "Arya Stark", 
+        "birthday": "11/25/1996" 
+    },
+    { 
+        "name": "John Snow", 
+        "birthday": "12/03/1989" 
+    },
+    { 
+        "name": "Tom Dsouza", 
+        "birthday": "27/10/1997" 
+    },
+    { 
+        "name": "Snow White", 
+        "birthday": "04/28/2005" 
+    },
+    { 
+        "name": "Eugene James", 
+        "birthday": "07/01/2005" 
+    },
+    { 
+        "name": "Bella Rose", 
+        "birthday": "10/08/2000" 
+    },
+    { 
+        "name": "John Drake", 
+        "birthday": "01/23/2015" 
+    },
+    { 
+        "name": "Snow Bella", 
+        "birthday": "04/08/2021" 
+    }
+];
+
+export { birthdayList };
